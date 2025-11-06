2cd5f08bc34dc946e67d14d52b7fd0145baf42ee
amara212 <96530980+AmaraX7@users.noreply.github.com>
2025-11-06 11:34:44 +0000

    Segunda versión del portfolio

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-// script.js
-console.log("Portfolio cargado correctamente");
+const links = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".content-section");
+
+// Mostrar solo la sección inicial (hero) al cargar
+sections.forEach(sec => {
+  if (sec.id !== "hero") sec.style.display = "none";
+});
+
+// Evento de navegación
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault(); // evitar scroll automático
+
+    const target = link.getAttribute("href").substring(1); // quita el #
+
+    sections.forEach(sec => {
+      if (sec.id === target) sec.style.display = "block";
+      else sec.style.display = "none";
+    });
+  });
+});
